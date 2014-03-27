58157daddce68964175445103ab9bcfdf16f7d22
Hal Norwood <justhal@gmail.com>
2014-03-27 15:23:38 -0400

    Handles scores that are multiples of 8

--- DailyProgrammer147/DailyProgrammer147/Program.cs
+++ DailyProgrammer147/DailyProgrammer147/Program.cs
@@ -10,8 +10,28 @@ namespace DailyProgrammer147
     {
         static void Main(string[] args)
         {
+            const int FIELD_GOAL_WITH_TWO_POINT_CONVERSION = 8;
+            bool validScore = false;
+
             Console.Write("Enter score: ");
-            int score = Int16.Parse(Console.ReadLine());
+            int score = int.Parse(Console.ReadLine());
+            
+            int remainder = score % FIELD_GOAL_WITH_TWO_POINT_CONVERSION;
+            if(remainder == 0)
+            {
+                validScore = true;
+            } else
+            {
+
+            }
+
+            if(validScore)
+            {
+                Console.WriteLine("Valid Score");
+            } else
+            {
+                Console.WriteLine("Invalid Score");
+            }
             Console.ReadKey();
         }
     }
