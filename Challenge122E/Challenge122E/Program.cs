fffb6cb47624270bcb42c3541a392d49039f6895
sullivanp <sullivanp@goldmail.etsu.edu>
2014-03-28 08:28:47 -0400

    Added Comment Header Block

---
+++ Challenge122E/Challenge122E/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * -------------------------------------------------------------------------------------------------------
+ * File name:       Challenge122E.sln<br/>
+ * Project name:    Challenge122E.cs<br/>
+ * -------------------------------------------------------------------------------------------------------
+ * Creator:         Patrick Sullivan, sullivanpatrickjohn@gmail.com<br/>			           
+ * Course:          Daily Programmer<br/>
+ * http://www.reddit.com/r/dailyprogrammer/comments/1aih0v/031813_challenge_122_easy_words_with_ordered/
+ * Creation Date:   28 Mar 2014<br/>
+  * ------------------------------------------------------------------------------------------------------
+ */
+
+
+namespace Challenge122E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
