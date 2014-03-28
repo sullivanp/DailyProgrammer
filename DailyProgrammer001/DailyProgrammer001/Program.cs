b360bf338a2c12fede5e6d485e090885f5d55871
Hal Norwood <justhal@gmail.com>
2014-03-28 17:45:44 -0400

    Solved, just need to add a header

---
+++ DailyProgrammer001/DailyProgrammer001/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgrammer001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Filename: ");
+            String filename = Console.ReadLine();
+
+            using(StreamReader sr = new StreamReader(filename))
+            {
+                String line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    if (areVowelsInOrder(line))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool areVowelsInOrder(string input)
+        {
+            bool inOrder = true;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int vowelIndex = 0;
+
+            for (int index = 0; index != input.Length; index++)
+            {
+                if (vowels.Contains(char.ToLower(input[index])))
+                {
+                    if (char.ToLower(input[index]) == vowels[vowelIndex])
+                    {
+                        vowelIndex++;
+                    } else
+                    {
+                        // Repeated or out-of-order vowel
+                        inOrder = false;
+                    }
+                }
+            }
+
+            inOrder &= (vowelIndex == vowels.Length);
+
+            return inOrder;
+        }
+    }
+}
