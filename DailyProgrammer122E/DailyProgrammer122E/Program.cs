55ce4cf16c11b07e8980b76afaffeeb4f1664f05
Hal Norwood <justhal@gmail.com>
2014-03-28 17:09:49 -0400

    initial commit

---
+++ DailyProgrammer122E/DailyProgrammer122E/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgrammer122E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input: ");
+            int input = int.Parse(Console.ReadLine());
+            int output = digitalRoot(input);
+        }
+
+        private static int digitalRoot(int input)
+        {
+            if(input == 0)
+            {
+                return 0;
+            }
+
+            int digits = (int) Math.Floor(Math.Log10(input));
+
+            return 0;
+        }
+    }
+}
