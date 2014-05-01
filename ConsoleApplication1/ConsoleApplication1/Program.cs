2b81f8aa5f6430f0df8ef326b4c0d20fb7d65245
Hal Norwood <justhal@gmail.com>
2014-05-01 15:07:21 -0400

    updated

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Author:  Hal Norwood <justhal@gmail.com>
+ * Date:    
+ * Spec:    
+ * Name:    
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
