db1d841209265a3564a1611b7ebebbc8f486095a
Hal Norwood <justhal@gmail.com>
2014-06-19 18:08:16 -0400

    Added Controller

---
+++ GraphMapper/GraphMapper/Controllers/GraphMapperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraphMapper.Controllers
+{
+    public class GraphMapperController : Controller
+    {
+        // GET: GraphMapper
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GraphMapper/GraphMapper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GraphMapper.MvcApplication" Language="C#" %>
