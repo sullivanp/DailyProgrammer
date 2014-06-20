e0d90afb8cd0a70698a520c80974c9697340287b
sullivanp <sullivanp@goldmail.etsu.edu>
2014-06-19 21:48:02 -0400

    Model Grid Properties

--- GraphMapper/GraphMapper/Models/Grid.cs
+++ GraphMapper/GraphMapper/Models/Grid.cs
@@ -6,18 +6,28 @@ namespace GraphMapper.Models
     public class Grid
     {
         // 2d array to store Tile objects
-        Tile[,] GridMap;
+        public Tile[,] GridMap { get; set; }
+        public int ID { get; set; }
+
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
+
       
         // default constructor
         public Grid()
         {
             GridMap = new Tile[12, 12]; // default grid 12x12
+            this.Rows = GridMap.GetLength(0);
+            this.Cols = GridMap.GetLength(1);
         }
 
         // overloaded constructor
         public Grid(int Rows, int Cols)
         {
             GridMap = new Tile[Rows, Cols];
+            this.Rows = Rows;
+            this.Cols = Cols;
         }
 
 
