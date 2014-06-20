d1cd14f57f2f0a3ede023fb4227eaa2f0351e285
sullivanp <sullivanp@goldmail.etsu.edu>
2014-06-19 22:39:29 -0400

    Ruined Everything

--- GraphMapper/GraphMapper/Models/Grid.cs
+++ GraphMapper/GraphMapper/Models/Grid.cs
@@ -7,19 +7,21 @@ namespace GraphMapper.Models
     public class Grid
     {
         // 2d array to store Tile objects
-        public virtual Tile[,] GridMap { get; set; }
+        //public virtual Tile[,] GridMap { get; set; }
         [Key]
         public int ID { get; set; }
 
         public int Rows { get; set; }
         public int Cols { get; set; }
 
+        public List<Tile> GridMap { get; set; }
        
       
         // default constructor
         public Grid()
         {
-            GridMap = new Tile[12, 12]; // default grid 12x12
+            //GridMap = new Tile[12, 12]; // default grid 12x12
+            GridMap = new List<Tile>(12);
             //this.Rows = GridMap.GetLength(0);
             //this.Cols = GridMap.GetLength(1);
             this.Rows = 12;
@@ -29,7 +31,8 @@ namespace GraphMapper.Models
         // overloaded constructor
         public Grid(int Rows, int Cols)
         {
-            GridMap = new Tile[Rows, Cols];
+            //GridMap = new Tile[Rows, Cols];
+            GridMap = new List<Tile>(Cols);
             this.Rows = Rows;
             this.Cols = Cols;
         }
