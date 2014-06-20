e5e27939a1f13a98a225b7899a335e3758a5fcca
sullivanp <sullivanp@goldmail.etsu.edu>
2014-06-19 20:58:52 -0400

    Initial Model Commit

---
+++ GraphMapper/GraphMapper/Models/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace GraphMapper.Models
+{
+    public class Grid
+    {
+        // 2d array to store Tile objects
+        Tile[,] Grid;
+      
+        // default constructor
+        public Grid()
+        {
+            Grid = new Tile[12, 12]; // default grid 12x12
+        }
+
+        // overloaded constructor
+        public Grid(int Rows, int Cols)
+        {
+            Grid = new Tile[Rows, Cols];
+        }
+
+
+    }
+
+
+    public class GridDBContext : DbContext
+    {
+        public DbSet<Grid> Grid { get; set; }
+    }
+
+}
