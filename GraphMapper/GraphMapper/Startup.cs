8ca82b1353e99178cf7578cecb57f2ac65f8e83a
Hal Norwood <justhal@gmail.com>
2014-06-23 13:33:05 -0400

    Created New GraphMapper Project

---
+++ GraphMapper/GraphMapper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GraphMapper.Startup))]
+namespace GraphMapper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
